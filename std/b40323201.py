40d9f651b2885bc7578b35dbe004e86baf2e8847
baconLEE <40323201@gm.nfu.edu.tw>
2014-12-29 21:31:37 +0800

    add 40323201 photo

--- std/b40323201.py
+++ std/b40323201.py
@@ -25,7 +25,7 @@ class Application(object):
         # 你的自評
         self.evaluation = [('Project 7', 80), ('Project 8', 80), ('Project 9', 80)]
         # 你的照片 url
-        self.photo_url = 'http://placekitten.com/g/350/300'
+        self.photo_url = 'https://copy.com/LvwRZQ0lXxyhhSrl'
         # 這裡是心得
         self.my_remark = """
         Computer Programming is good course
