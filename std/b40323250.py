5925759f860d8338313deb8311bf90c9efb1457c
40323250 <b850617@yahoo.com.tw>
2014-12-29 09:31:40 +0800

    f

--- std/b40323250.py
+++ std/b40323250.py
@@ -27,7 +27,7 @@ class Application(object):
         # 你的照片 url
         self.photo_url = 'http://placekitten.com/g/350/300'
         # 這裡是心得
-        self.my_remark = "123"
+        self.my_remark = """
         Computer Programming is good course
         """
 
