d6a217e6458ef02cbb734ec2587d4cd145b20b4e
歐芳汝 <40323207@gm.nfu.edu.tw>
2014-12-29 19:28:23 +0800

    add b40323207

--- std/b40323207.py
+++ std/b40323207.py
@@ -23,7 +23,7 @@ class Application(object):
         # 你的 openshift app
         self.openshift_url = 'http://cpb-nfutaiwan.rhcloud.com/'
         # 你的自評
-        self.evaluation = [('Project 7', 70), ('Project 8', 70), ('Project 9', 7040323207)]
+        self.evaluation = [('Project 7', 70), ('Project 8', 70), ('Project 9', 70)]
         # 你的照片 url
         self.photo_url = 'http://placekitten.com/g/350/300'
         # 這裡是心得
