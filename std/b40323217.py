14cfbd4e6e567b8f44a4d92526c071e124d1360f
Openandgit <40323217@gm.nfu.edu.tw>
2015-01-04 10:52:52 +0800

    test

--- std/b40323217.py
+++ std/b40323217.py
@@ -27,8 +27,7 @@ class Application(object):
         # 你的照片 url
         self.photo_url = 'https://copy.com/4vbQRPcklDdC'
         # 這裡是心得
-        self.my_remark = """
-       123 < br /> 123 
+        self.my_remark = """ 123<br/>
  
         """
 
