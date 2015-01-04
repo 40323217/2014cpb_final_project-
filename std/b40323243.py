1f65e987a10ef0e9bd5c62e69d4c5bfb6e8fc20f
z22018315 <40323243@gm.nfu.edu.tw>
2015-01-04 22:57:04 +0800

    add 40323243

--- std/b40323243.py
+++ std/b40323243.py
@@ -17,18 +17,18 @@ class Application(object):
         # 你的學號
         self.number = '40323243'
         # 你的班級
-        self.classes = 'nfu'
+        self.classes = '四設一乙'
         # 你的 github repository url
-        self.github_repo_url = 'https://github.com/Openandgit/2014cpb_final_project-'
+        self.github_repo_url = 'https://github.com/40323243/--2014cp_project_40323243'
         # 你的 openshift app
-        self.openshift_url = 'http://cpb-nfutaiwan.rhcloud.com/'
+        self.openshift_url = 'http://2014project-2014cpfall.rhcloud.com/'
         # 你的自評
         self.evaluation = [('Project 7', 80), ('Project 8', 90), ('Project 9', 100)]
         # 你的照片 url
-        self.photo_url = 'http://placekitten.com/g/350/300'
+        self.photo_url = 'https://copy.com/XWagU0N9witKQKFc'
         # 這裡是心得
         self.my_remark = """
-        Computer Programming is good course
+        謝謝老師的指導，讓我們學習如何製作python。
         """
 
     #@+node:lee.20141223114246.43: *3* def use_template
