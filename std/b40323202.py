44341620c3d0d3d81e3c4fc4e012a4acac33e7eb
歐芳汝 <40323207@gm.nfu.edu.tw>
2015-01-04 20:54:06 +0800

    123

--- std/b40323202.py
+++ std/b40323202.py
@@ -17,18 +17,18 @@ class Application(object):
         # 你的學號
         self.number = '40323202'
         # 你的班級
-        self.classes = 'nfu'
+        self.classes = '設計一乙'
         # 你的 github repository url
-        self.github_repo_url = 'https://github.com/Openandgit/2014cpb_final_project-'
+        self.github_repo_url = 'https://github.com/40323202/2014cp_project_40323202.git'
         # 你的 openshift app
-        self.openshift_url = 'http://cpb-nfutaiwan.rhcloud.com/'
+        self.openshift_url = 'http://b40323202-40323202.rhcloud.com/doCheck'
         # 你的自評
-        self.evaluation = [('Project 7', 80), ('Project 8', 90), ('Project 9', 100)]
+        self.evaluation = [('Project 7', 95), ('Project 8', 95), ('Project 9', 95)]
         # 你的照片 url
-        self.photo_url = 'http://placekitten.com/g/350/300'
+        self.photo_url = 'https://copy.com/MWbKHNXOSyj6Pkvd'
         # 這裡是心得
         self.my_remark = """
-        Computer Programming is good course
+        計算機程式是一門很深奧的課程，很榮幸有同學，老師和助教教我，讓我從原來的沒有概念經過大家的幫助建立起了一些概念，獲益良多。
         """
 
     #@+node:lee.20141223114246.43: *3* def use_template
