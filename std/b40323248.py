7c03c50148bff9bb9a57f36ba440d639528e25af
chiaming yen <chiamingyen@gmail.com>
2015-01-04 22:08:40 +0800

    40323248-2

--- std/b40323248.py
+++ std/b40323248.py
@@ -17,15 +17,15 @@ class Application(object):
         # 你的學號
         self.number = '40323248'
         # 你的班級
-        self.classes = 'nfu'
+        self.classes = '四設計一乙'
         # 你的 github repository url
-        self.github_repo_url = 'https://github.com/Openandgit/2014cpb_final_project-'
+        self.github_repo_url = 'https://github.com/YANG-CHENG-RU/40323248-2'
         # 你的 openshift app
-        self.openshift_url = 'http://cpb-nfutaiwan.rhcloud.com/'
+        self.openshift_url = 'http://new40323248-40323248.rhcloud.com/'
         # 你的自評
-        self.evaluation = [('Project 7', 80), ('Project 8', 90), ('Project 9', 100)]
+        self.evaluation = [('Project 7', 75), ('Project 8', 80), ('Project 9', 80)]
         # 你的照片 url
-        self.photo_url = 'http://placekitten.com/g/350/300'
+        self.photo_url = 'https://copy.com/hYXO7cYEI8of'
         # 這裡是心得
         self.my_remark = """
         Computer Programming is good course
